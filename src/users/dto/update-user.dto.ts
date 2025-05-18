9b928ebd38dc64655fed880fc910e8f8ccc61ad5
ferin79 <ferinpatel79@gmail.com>
2025-05-18 18:19:38 -0300

    feat: refactor UpdateUserDto by removing email field and unused imports ✂️

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,20 +1,13 @@
-import { PartialType, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-import { Transform, Type } from 'class-transformer';
-import { IsEmail, IsOptional, MinLength } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsOptional, MinLength } from 'class-validator';
 import { FileDto } from '../../files/dto/file.dto';
 import { RoleDto } from '../../roles/dto/role.dto';
 import { StatusDto } from '../../statuses/dto/status.dto';
-import { lowerCaseTransformer } from '../../utils/transformers/lower-case.transformer';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @ApiPropertyOptional({ example: 'test1@example.com', type: String })
-  @Transform(lowerCaseTransformer)
-  @IsOptional()
-  @IsEmail()
-  email?: string | null;
-
   @ApiPropertyOptional()
   @IsOptional()
   @MinLength(6)
